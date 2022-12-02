236d86e479a3ad0cae1e91ea7059ace60dd9d82c
Nico Unterburger <nico-unterburger@live.de>
2022-12-02 18:48:04 +0100

    Create sendSMTPmail.py

---
+++ sendSMTPmail.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+msg = MIMEMultipart()
+
+usernmae = "user@name.de"
+password = "password"
+mailFrom = "mailfrom@provider.de"
+mailTo = "mailto@provider.de"
+server = smtplib.SMTP('mail.provider.de', 25)
+
+#Next, log in to the server
+server.login(username, password)
+
+#Send the mail
+msg['From'] = mailFrom
+msg['To'] = mailTo
+msg['Subject'] = 'Betreff'
+
+
+server.sendmail(mailFrom, mailTo, msg.as_string())
